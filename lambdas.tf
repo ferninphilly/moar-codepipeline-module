6387fc5852ce27323afc750a26fa33387de3e52e
David Wood <david@codices.io>
2022-03-30 17:08:14 +0100

    Fix slack deployment notifications for Studio client
    This uses the newly enabled lambda_function_handler argument of the lambda module to allow the lambda function handler to be given a generic filename (instead of the previous, which was statically determined to only work when `client = "authentication"`), while keeping the function name unique.

--- lambdas.tf
+++ lambdas.tf
@@ -3,6 +3,7 @@ module "slack_notify" {
   stack_name                               = "${var.client}-${var.environment}"
   lambda_function_description              = "${var.client} lambda to notify slack on pushes"
   lambda_binary_name                       = "moar-send-slack-notifications"
+  lambda_function_handler                  = "moar-send-slack-notifications.handler"
   lambda_function_name                     = "moar-send-slack-notifications-${var.client}"
   lambda_function_source_base_path         = "${path.module}/lambdas_code"
   lambda_function_existing_execute_role    = aws_iam_role.slack_notify_lambda_role.arn
