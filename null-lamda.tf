c8d172a5bed2a1b6fdc094fa32b8e8e27689b9df
Ben Sewell <ben@calc-i.com>
2022-07-13 12:30:56 +0100

    Try to correct null lambda permissions and handler

--- null-lamda.tf
+++ null-lamda.tf
@@ -1,26 +1,6 @@
 /* Null lambda - for removing pipeline stages
  */
 
-resource "aws_iam_role" "iam_for_null_lambda" {
-  name_prefix = "iam_for_null_lambda"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
 resource "aws_lambda_function" "null_lambda" {
   // A bit of a hack to allow us to use the same ZIP file everywhere.
   filename      = ".terraform/modules/codepipeline_module/null_lambda_function.zip"
@@ -28,4 +8,27 @@ resource "aws_lambda_function" "null_lambda" {
   role          = aws_iam_role.iam_for_null_lambda.arn
   handler       = "index.js"
   runtime       = "nodejs14.x"
+}
+
+
+resource "aws_iam_role" "iam_for_null_lambda" {
+  name = "serverless_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "policy_for_null_lambda" {
+  role       = aws_iam_role.iam_for_null_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
