0e34b29c5606b83875fd559d39861ff48377df10
Fernando Pombeiro <fernpombeiro@yahoo.com>
2023-07-11 17:14:06 +0100

    Updated

--- codebuild-linter.tf
+++ codebuild-linter.tf
@@ -21,7 +21,6 @@ resource "aws_codebuild_project" "linter" {
   source {
     type = "CODEPIPELINE"
     buildspec = templatefile("${path.module}/tpls/lintspec.yml", {
-      TF_DIR       = local.TF_DIR
       GIT_TOKEN    = local.git_token
       CURRENT_DATE = formatdate("YYYYMMDDhhmm", timestamp())
       CLIENT       = var.client
